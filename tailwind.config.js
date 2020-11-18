b49404f8124f6d4d2693de724a5c6f8cba0f2740
Akbar Aditama <Aditamaakbar1108@gmail.com>
2020-11-18 18:23:08 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    colors: {
+      primary: "#0e9aa7",
+      primaryDark: "#0a6c75",
+      white: {
+        100: "#FFF",
+        200: "#E0E0E0",
+        300: "#C0C0C0",
+      },
+      black: {
+        100: "#000",
+        200: "#303030",
+        300: "#505050",
+      },
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
